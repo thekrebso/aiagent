24714354cc55ce7c7683d372a6597f95a8a91c8c
krebso <krebso@krebso.pl>
2025-10-21 23:17:48 +0200

    hardcoded test generate response

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <prompt>")
+        exit(1) 
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=sys.argv[1]
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
