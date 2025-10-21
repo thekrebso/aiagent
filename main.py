3a6850323c6f91915d2041b23569d2991865dfec
krebso <krebso@krebso.pl>
2025-10-21 23:32:01 +0200

    add optional --verbose flag for extra information

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ from google.genai import types
 def main():
     load_dotenv()
 
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <prompt>")
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print(f"Usage: {sys.argv[0]} <prompt> [flags]")
+        print(f"    --verbose   prints additional info")
         exit(1) 
     
     user_prompt = sys.argv[1]
+    
+    verbose = False
+    if "--verbose" in sys.argv:
+        verbose = True
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")    
     
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -27,8 +35,10 @@ def main():
     )
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
