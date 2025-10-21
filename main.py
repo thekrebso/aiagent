f1f79014d88b77e4f1e2a8a6f3aad56576345e4d
krebso <krebso@krebso.pl>
2025-10-21 23:31:38 +0200

    change message into a list for future conversation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -11,13 +12,18 @@ def main():
         print(f"Usage: {sys.argv[0]} <prompt>")
         exit(1) 
     
+    user_prompt = sys.argv[1]
+    
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1]
+        contents=messages
     )
 
     print(response.text)
