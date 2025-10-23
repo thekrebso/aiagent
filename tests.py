e1ae5569c1c4d74838a449705a1e9d2ef8b881d0
krebso <krebso@krebso.pl>
2025-10-23 23:29:05 +0200

    test get_file_content()

--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
+from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
 
+print("Result for 'main.py' file:")
+print(get_file_content("calculator", "main.py"))
 
-print("Result for current directory:")
-print(get_files_info("calculator", "."))
+print("\nResult for 'pkg/calculator.py' file:")
+print(get_file_content("calculator", "pkg/calculator.py"))
 
-print("Result for 'pkg' directory")
-print(get_files_info("calculator", "pkg"))
+print("\nResult for listing files in 'pkg' directory:")
+print(get_file_content("calculator", "/bin/cat"))
 
-print("Result for '/bin' directory")
-print(get_files_info("calculator", "/bin"))
-
-print("Result for '../' directory")
-print(get_files_info("calculator", "../"))
+print("\nResult for non-existing file:")
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
